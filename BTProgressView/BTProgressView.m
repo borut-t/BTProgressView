bc261afbda1f3d1ff265900042e906721fcf7e8e
Borut Tomažin <borut.tomazin@gmail.com>
2013-08-30 08:56:20 +0200

    Added podspecs, updated licence.

--- BTProgressView/BTProgressView.m
+++ BTProgressView/BTProgressView.m
@@ -1,9 +1,33 @@
 //
 //  BTProgressView.m
-//  BTProgressView
 //
-//  Created by Borut Tomažin on 2/21/13.
-//  Copyright (c) 2013 Borut Tomazin. All rights reserved.
+//  Version 1.0.0
+//
+//  Created by Borut Tomazin on 2/21/2013.
+//  Copyright 2013 Borut Tomazin
+//
+//  Distributed under the permissive zlib License
+//  Get the latest version from here:
+//
+//  https://github.com/borut-t/BTProgressView
+//
+//  This software is provided 'as-is', without any express or implied
+//  warranty.  In no event will the authors be held liable for any damages
+//  arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//  claim that you wrote the original software. If you use this software
+//  in a product, an acknowledgment in the product documentation would be
+//  appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be
+//  misrepresented as being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
 //
 
 #import "BTProgressView.h"
